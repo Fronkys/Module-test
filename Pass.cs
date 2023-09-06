2a1ee26170bd32be68521c2b6b49638361a3b692
Максим Каверин <k1sper777@mail.ru>
2023-09-06 15:59:48 +0300

    Добавьте файлы проекта.

---
+++ Pass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Module_test
+{
+    public class Pass
+    {
+        public static int Password(string word)
+        {
+            
+            int score = 0;
+            
+            foreach(char c in word)
+            {
+                if (char.IsDigit(c))
+                {
+                    score++;
+                    break;
+                }
+               
+            }
+            foreach (char c in word)
+            {
+                if (char.IsLower(c))        
+                {
+                    score++;
+                    break;
+                   
+                }
+                
+            }
+            foreach (char c in word)
+            {
+                if (char.IsUpper(c))        
+                {
+                    score++;
+                    break;
+                }
+                
+            }
+            if (Regex.IsMatch(word, @"[-!@#№$|[{\]};%^:?&*()_+=./<>,}`~]"))
+            { 
+                score++;
+            }
+            if (word.Length > 7) 
+            {
+                score++;    
+            }
+                return score;
+        }
+    }
+}
